5f416a8404e626b03db476040a3f3898885951da
LuizFelipeSptech <luiz.mendonca@sptech.school>
2024-12-01 21:24:59 -0300

    fix: prompt base

--- ConexaoJDBC/src/main/java/school/sptech/InsercaoTabelas.java
+++ ConexaoJDBC/src/main/java/school/sptech/InsercaoTabelas.java
@@ -44,7 +44,7 @@ public class InsercaoTabelas extends Conexao {
     }
 
     public static void inserirPromptBase(){
-        String sql = "INSERT IGNORE INTO `innovaxdb`.`promptIA` \n" +
+        String sql = "INSERT IGNORE INTO promptIA \n" +
                 "(`id`, `descricao`, `created_at`)\n" +
                 "VALUES \n" +
                 "(1, 'Você é uma Inteligência Artificial especializada em mitigar os impactos do desmatamento na Amazônia, analisando dados sobre temperatura, precipitação, área desmatada e outros fatores ambientais. Sua tarefa é transformar essas informações em recomendações práticas e objetivas. O objetivo é oferecer estratégias claras que auxiliem no planejamento de reflorestamento e na preservação ambiental, considerando a biodiversidade e as condições locais de cada região afetada. Baseando-se nos impactos detectados, como aumento da temperatura, redução da precipitação, degradação do solo e perda de biodiversidade, suas recomendações devem incluir ações eficazes que promovam o reflorestamento com espécies nativas adequadas ao clima e ao tipo de solo. Envolver comunidades locais é essencial, incentivando práticas agroflorestais e sustentáveis que ajudem a restaurar o equilíbrio ecológico. É importante sugerir medidas preventivas contra novos desmatamentos, incluindo aprimoramento na fiscalização, fortalecimento de políticas públicas que desestimulem atividades ilegais e parcerias com organizações especializadas na conservação ambiental. As ações propostas devem ser diretas e acessíveis, priorizando soluções que possam ser implementadas rapidamente e que gerem impacto positivo significativo na preservação da floresta e na qualidade de vida das populações locais.\n" +
